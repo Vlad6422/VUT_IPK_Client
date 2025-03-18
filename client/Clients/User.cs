b8d53bae33190bf2d45ea38caee9e090ce322e84
Malashchuk Vladyslav <xmalas04@vutbr.cz>
2025-03-18 21:16:01 +0100

    feat: User Class provides class and methods of it, to hadle username,secret,displayname and change it + validate

---
+++ client/Clients/User.cs
@@ -0,0 +1,58 @@
+﻿namespace ipk25_chat.Clients
+{
+    public class User
+    {
+        protected string _username { get; set; } = String.Empty;
+        protected string _secret { get; set; } = String.Empty;
+        protected string _displayName { get; set; } = String.Empty;
+        protected bool ChangeUserName(string username)
+        {
+            if (username.Length > 20 || !System.Text.RegularExpressions.Regex.IsMatch(username, @"^[A-Za-z0-9\-]+$"))
+            {
+                WriteInternalError("Too Big UserName or Incorect");
+                return false;
+            }
+            else
+            {
+                _username = username;
+            }
+            return true;
+        }
+        protected bool ChangeSecret(string secret)
+        {
+            if (secret.Length > 128 || !System.Text.RegularExpressions.Regex.IsMatch(secret, @"^[A-Za-z0-9\-]+$"))
+            {
+                WriteInternalError("Too Big Secret or Incorect");
+                return false;
+            }
+            else
+            {
+                _secret = secret;
+            }
+            return true;
+
+        }
+        protected bool ChangeDisplayName(string newName)
+        {
+            if (!newName.All(c => c >= 0x21 && c <= 0x7E))
+            {
+                WriteInternalError("DisplayName contains invalid characters");
+                return false;
+            }
+            if (_displayName.Length > 20)
+            {
+                WriteInternalError("Too Big DisplayName or Incorect");
+                return false;
+            }
+            else
+            {
+                _displayName = newName;
+            }
+            return true;
+        }
+        protected void WriteInternalError(string error)
+        {
+            Console.WriteLine($"ERROR: {error}");
+        }
+    }
+}
