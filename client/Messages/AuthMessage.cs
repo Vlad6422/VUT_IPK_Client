a7a481168a14cc289d79bc1a82a2c7c3514999c4
Malashchuk Vladyslav <xmalas04@vutbr.cz>
2025-03-18 21:17:21 +0100

    feat : Auth Message

---
+++ client/Messages/AuthMessage.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace ipk25_chat.Messages
+{
+    public class AuthMessage
+    {
+        public byte MessageType { get; set; } = 0x02;
+        public ushort MessageID { get; set; }
+        public string Username { get; set; }
+        public string DisplayName { get; set; }
+        public string Secret { get; set; }
+
+        // Constructor
+        public AuthMessage(ushort MessageID, string Username, string DisplayName, string Secret)
+        {
+            this.MessageID = MessageID;
+            this.Username = Username;
+            this.DisplayName = DisplayName;
+            this.Secret = Secret;
+        }
+
+        // Method to get the bytes of the message
+        public byte[] GET()
+        {
+            List<byte> bytes = new List<byte>();
+            bytes.Add(MessageType);
+            bytes.AddRange(BitConverter.GetBytes(MessageID));
+            bytes.AddRange(Encoding.ASCII.GetBytes(Username));
+            bytes.Add(0);
+            bytes.AddRange(Encoding.ASCII.GetBytes(DisplayName));
+            bytes.Add(0);
+            bytes.AddRange(Encoding.ASCII.GetBytes(Secret));
+            bytes.Add(0);
+            return bytes.ToArray();
+        }
+    }
+}
