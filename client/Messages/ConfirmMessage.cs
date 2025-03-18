e1633aae53193cc39e7b2ad2d927be3406abc2ec
Malashchuk Vladyslav <xmalas04@vutbr.cz>
2025-03-18 21:17:09 +0100

    feat : Confirm Message

---
+++ client/Messages/ConfirmMessage.cs
@@ -0,0 +1,23 @@
+﻿namespace ipk25_chat.Messages
+{
+    public class ConfirmMessage
+    {
+        public byte MessageType { get; set; } = 0x00;
+        public ushort RefMessageID { get; set; }
+
+        // Constructor
+        public ConfirmMessage(ushort RefMessageID)
+        {
+            this.RefMessageID = RefMessageID;
+        }
+
+        // Method to get the bytes of the message
+        public byte[] GET()
+        {
+            List<byte> bytes = new List<byte>();
+            bytes.Add(MessageType);
+            bytes.AddRange(BitConverter.GetBytes(RefMessageID));
+            return bytes.ToArray();
+        }
+    }
+}
