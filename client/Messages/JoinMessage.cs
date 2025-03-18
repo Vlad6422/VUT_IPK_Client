ee21dedf4c90115271d17ac4dcba508dd6301541
Malashchuk Vladyslav <xmalas04@vutbr.cz>
2025-03-18 21:16:57 +0100

    feat : JOIN Message

---
+++ client/Messages/JoinMessage.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace ipk25_chat.Messages
+{
+    public class JoinMessage
+    {
+        public byte MessageType { get; set; } = 0x03;
+        public ushort MessageID { get; set; }
+        public string ChannelID { get; set; }
+        public string DisplayName { get; set; }
+
+        public JoinMessage(ushort messageID, string channelID, string displayName)
+        {
+            MessageID = messageID;
+            ChannelID = channelID;
+            DisplayName = displayName;
+        }
+
+        public byte[] GET()
+        {
+            List<byte> bytes = new List<byte>();
+            bytes.Add(MessageType);
+            bytes.AddRange(BitConverter.GetBytes(MessageID));
+            bytes.AddRange(Encoding.ASCII.GetBytes(ChannelID));
+            bytes.Add(0);
+            bytes.AddRange(Encoding.ASCII.GetBytes(DisplayName));
+            bytes.Add(0);
+            return bytes.ToArray();
+        }
+    }
+}
